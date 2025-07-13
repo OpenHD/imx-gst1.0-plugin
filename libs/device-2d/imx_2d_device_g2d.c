14e408b44ccbb6433dc31035a3b3c8db328ffe0a
Raphael <raphael@openhdfpv.org>
2025-07-13 16:09:46 +0200

    Update imx_2d_device_g2d.c

--- libs/device-2d/imx_2d_device_g2d.c
+++ libs/device-2d/imx_2d_device_g2d.c
@@ -95,8 +95,9 @@ static G2dFmtMap g2d_fmts_map_dpu[] = {
     {GST_VIDEO_FORMAT_UYVY,   G2D_UYVY,     16},
     {GST_VIDEO_FORMAT_YUY2,   G2D_YUYV,     16},
     {GST_VIDEO_FORMAT_NV12,   G2D_NV12,     12},
-    {GST_VIDEO_FORMAT_GRAY8,  G2D_GRAY8,    8},
-
+    #ifdef G2D_GRAY8
+    {GST_VIDEO_FORMAT_GRAY8, G2D_GRAY8, 8},
+    #endif
     //this only for separate YUV format and RGB format
     {GST_VIDEO_FORMAT_UNKNOWN, -1,          1},
 
